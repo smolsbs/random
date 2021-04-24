0a308e17ea82638eae4054dc22fcbf31e659fdf7
Sara <smol.sarabs@gmail.com>
2021-04-25 00:32:41 +0100

    amipwned updated

--- amipwned.py
+++ amipwned.py
@@ -1,39 +1,30 @@
-#! python
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 # imports
 import sys
+from argparse import ArgumentParser
 from hashlib import sha1
+
+try:
+    import pyperclip
+except ModuleNotFoundError:
+    print('Module pyperclip not installed. Please install it.')
+    sys.exit(1)
 from requests import get
-from tkinter import Tk
-
-def help():
-    return """Usage:
-    python amipwned.py \"password\";
-    python amipwned.py [-c|--clipboard]"""
 
 
-def hashMyPass():
+def hashMyPass(pwd, verbose=False):
     """Computes a password into it's SHA-1 hash and sends the first 5 chars to haveibeenpwned API, then checks all the responses from the
     server against the password hash to see if it's in the haveibeenpwned database."""
 
-
-
-    # check if user has a password in the arguments
-    # if not, throw an exception
-    if len(sys.argv)  != 2:
-        raise Exception("No argument passed.\n\n{}".format(help()))
-    
-    if sys.argv[1].lower() in  ['-c','--clipboard']:
-        pwd = Tk().clipboard_get().encode('utf-8')
-    else:
-        #  prepares password string to utf-8
-        pwd = sys.argv[1].encode('utf-8')
-
     # hashes the password and returns it's hexdigest
     hashedPwd = sha1(pwd).hexdigest()
+    if verbose:
+        n_pwd = len(pwd)
+        print(f'[*] Size of password: {n_pwd}')
+        print(f'[*] First 6 chars of the hashed password: {hashedPwd[:6]}...')
 
-    # firsts 5 chars of the hash
+    # firsts 6 chars of the hash
     hashPre = hashedPwd[:5]
 
     # sends a GET request to the API and retrieves the response
@@ -41,10 +32,15 @@ def hashMyPass():
     req = get('https://api.pwnedpasswords.com/range/' + hashPre)
     if req.status_code != 200:
         print("Unable to retrieve a response. Got a {:%d} response".formar(req.status_code))
-
+    if verbose:
+        print(f'[*] 200 OK from api.pwnedpasswords.com')
     # splits every hash got from the server and iterates over it, checking against our password hash
-    for h in req.text.split('\n'):
-        
+    all_hashes = req.text.split('\n')
+    if verbose:
+        n_hashes = len(all_hashes)
+        print(f'[*] number of hashes returned: {n_hashes}')
+
+    for h in all_hashes:
         h = h.split(':')        # format=> hash:nOccurrences
 
         if hashedPwd == hashPre + h[0].lower():
@@ -55,4 +51,21 @@ def hashMyPass():
 
 
 if __name__ == '__main__':
-    hashMyPass()
+    args = ArgumentParser()
+    args.add_argument('-c', dest='clip', action='store_true', help='get password from clipboard')
+    args.add_argument('-t', dest='pw',action='store', help='input the password as an argument')
+    args.add_argument('-v', '--verbose', action='store_true', help='Increases the verbosity of the output')
+    parser = args.parse_args()
+
+    content = ''
+    verb = False
+
+    if parser.clip:
+        content = pyperclip.paste().encode('utf-8')
+    elif parser.pw:
+        content = parser.pw.encode('utf-8')
+
+    if parser.verbose:
+        verb = True
+
+    hashMyPass(content, verb)
